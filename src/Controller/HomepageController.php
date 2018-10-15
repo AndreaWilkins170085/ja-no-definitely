44f5aa25ee2f08d51682d116a12990defd37f1b3
Ashleigh Parsons <170074@virtualwindow.co.za>
2018-10-15 23:10:50 +0200

    Homepage Controller added

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/HomepageController.php
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\UserProfile;
+
+class HomepageController extends AbstractController
+{
+    /**
+    * @Route("/", name="home_view")
+    */
+
+    public function viewHomepage()
+    {
+    $view = 'home.html.twig';
+    return $this->render($view);
+    }
+}
+
+?>
