e4e7eca8c6187e50b546b590a562ca804551bf4b
Ashleigh Parsons <170074@virtualwindow.co.za>
2018-10-22 16:40:48 +0200

    Basic Question and Answer forms

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
+use App\Entity\Questions;
+use App\Form\Question;
+use App\Entity\Answers;
+use App\Form\Answer;
 
 class HomeController extends AbstractController
 {
@@ -14,10 +18,19 @@ class HomeController extends AbstractController
     * @Route("/home", name="home_view")
     */
 
-    public function viewHomepage()
+    public function viewHomepage(Request $request)
     {
+    $question = new Questions();
+    $questionForm = $this->createForm(Question::class, $question);
+    $questionForm->handleRequest($request);
+
+    $answer = new Answers();
+    $answerForm = $this->createForm(Answer::class, $answer);
+    $answerForm->handleRequest($request);
+
     $view = 'home.html.twig';
-    return $this->render($view);
+    $model = array('questionForm' => $questionForm->createView(), 'answerForm' => $answerForm->createView());
+    return $this->render($view, $model);
     }
 }
 
---
+++ src/Form/Answer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class Answer extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('answer_author', HiddenType::class)
+            ->add('answer_date', HiddenType::class)
+            ->add('question_id', HiddenType::class)
+            ->add('answer_text', TextType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Answer!']);
+        ;
+    }
+}
+?>
