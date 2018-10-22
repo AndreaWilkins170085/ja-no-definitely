42d6124e604eb435dd2bcf3e647c52fb4c9538bd
Leo Kuyper <170071@virtualwindow.co.za>
2018-10-22 23:29:47 +0200

    Simple Hashing demo --will be removed

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,12 +2,12 @@
 // src/Controller/LoginController.php
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+
 use App\Form\Login;
 use App\Form\Register;
 
@@ -22,12 +22,54 @@ class LoginController extends AbstractController
     $user = new User();
     $loginForm = $this->createForm(Login::class, $user);
     $loginForm->handleRequest($request);
+
+    if ($loginForm->isSubmitted() && $loginForm->isValid()) {
+        // $form->getData() holds the submitted values
+        $user = $loginForm->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        
+
+        return $this->redirectToRoute('home_view');
+    }
+
     $registerForm = $this->createForm(Register::class, $user);
     $registerForm->handleRequest($request);
+
+    if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+        // $form->getData() holds the submitted values
+        $user = $registerForm->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+        
+
+        return $this->redirectToRoute('home_view');
+    }
+    
+    
     $view = 'login.html.twig';
     $model = array('loginForm' => $loginForm->createView(), 'registerForm' => $registerForm->createView());
     return $this->render($view, $model);
     }
 
 }
+
+
+
+$test = hash( "sha256", "data");
+echo $test;
+echo '<br />';
+$test2 = hash( "sha256", "data");
+echo $test;
+echo '<br />';
+
+if ($test === $test2){
+    echo "true";
+}else{
+    echo "false";
+}
+
 ?>
