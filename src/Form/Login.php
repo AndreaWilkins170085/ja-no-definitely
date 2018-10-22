e0730a90a44f1f913292de290b1ba2b1d7fca278
AndreaWilkins170085 <32729395+AndreaWilkins170085@users.noreply.github.com>
2018-10-22 13:39:56 +0200

    Login and Register classes implemented

---
+++ src/Form/Login.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class Login extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Login']);
+        ;
+    }
+}
+?>
---
+++ src/Form/Register.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class Register extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('loginFB', SubmitType::class, ['label' => 'Sign Up with Facebook'])
+            ->add('submit', SubmitType::class, ['label' => 'Register']);
+        ;
+    }
+}
+?>
