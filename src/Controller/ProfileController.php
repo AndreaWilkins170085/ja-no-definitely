c4d4a969aa3e7855ad75282566c2d64ebb0b00b2
Ashleigh Parsons <170074@virtualwindow.co.za>
2018-10-24 20:33:31 +0200

    starting to navigate to specific user, made user repository

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,26 +6,31 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\UserProfile;
+use App\Entity\User;
 use App\Entity\Category;
 
 class ProfileController extends AbstractController
 {
+    
     /**
-    * @Route("/profile", name="profile_view")
+    * @Route("/profile/{id}", name="profile_view")
     */
-
-    public function viewProfilepage()
+    public function viewProfilepage($id = "1")
     {
+        $userId = (int) $id;
+        $user = $this->getDoctrine()
+        ->getRepository(User::class)
+        ->find($userId);
 
         $categorys = $this->getDoctrine()
         ->getRepository(Category::class)
-        ->findAll(); 
+        ->findAll();
 
         $view = 'profile.html.twig';
-        $model = array('categorys' => $categorys);
+        $model = array('user' => $user, 'categorys' => $categorys);
         return $this->render($view, $model);
     }
+
 }
 
 ?>
